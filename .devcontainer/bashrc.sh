029bd42479d00da3465024b41aef1f2e59fe70a5
Filip Sufitchi <fsufitchi@gmail.com>
2024-12-22 18:37:14 +0000

    Working devcontainer with root user

--- .devcontainer/bashrc.sh
+++ .devcontainer/bashrc.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+umask 022
+
+export LS_OPTIONS='--color=auto'
+
+eval "$(dircolors)"
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
+
+# Some more alias to avoid making mistakes:
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Drop-ins
 if [ -d ~/.bashrc.d ]; then
     for rc in ~/.bashrc.d/*; do
         if [ -f "$rc" ]; then
